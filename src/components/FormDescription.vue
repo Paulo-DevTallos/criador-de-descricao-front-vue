ae44eb22d75502a259e2de4f2c65d13d025784b6
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-07-16 19:04:23 -0300

    feat: create header and form components

---
+++ src/components/FormDescription.vue
@@ -0,0 +1,86 @@
+<template>
+  <form @submit.prevent="$emit('submitDescription', description)">
+    <h3>Insira aqui o nome do seu melhor item!</h3>
+    <div class="description-generate">
+      <label>Produto</label>
+      <input type="text" v-model="description" placeholder="nome do produto">
+      <button type="submit">Criar descrição</button>
+    </div>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "FormDescription",
+  emits: ['submitDescription'],
+  data() {
+    return {
+      description: '',
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+form {
+  height: 100%;
+
+  h3 {
+    font-size: 1.7rem;
+    color: #0d0d0d;
+    font-weight: 600;
+    letter-spacing: -1px;
+  }
+  .description-generate {
+    display: flex;
+    flex-direction: column;
+
+    label {
+      margin-bottom: 5px;
+      color: #0d0d0d;
+      font-weight: normal;
+      font-size: 1.2rem;
+    }
+
+    input {
+      padding: 10px;
+      font-size: 1rem;
+      color: #0d0d0d;
+      height: 45px;
+      border-radius: 8px;
+      outline: none;
+      border: 1px solid #dbdbdb;
+      margin-bottom: 10px;
+      transition: .2s ease-in-out;
+
+      &:focus {
+        border-color: #0d0d0d;
+      }
+    }
+
+    button {
+      width: 180px;
+      height: 40px;
+      border-radius: 12px;
+      border: 1px solid #0d0d0d;
+      background: #0d0d0d;
+      color: #fff;
+      font-size: 1rem;
+      cursor: pointer;
+      transition: .2s ease-in-out;
+
+      &:hover {
+        border-color: gray;
+        background-color: gray;
+      }
+
+      &:active {
+        background-color: #0d0d0d;
+        transition: 0s;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <div class="logo-container">
+      <img :src="logo" alt="Logo ProDescritpion">
+    </div>
+    <div>
+      <button>Crie sua conta</button>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import logo from '@/assets/img/ProDescriber-removebg-preview.png';
+
+export default defineComponent({
+  name: "MainHeader",
+  data() {
+    return {
+      logo,
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .logo-container {
+    width: 220px;
+    padding: 30px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    img {
+      width: 100%;
+    }
+  }
+}
+</style>
