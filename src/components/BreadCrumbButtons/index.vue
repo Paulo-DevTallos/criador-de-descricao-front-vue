e7baed31f32723f5d045bbb57c176d8fbbe79bc6
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-09-23 16:46:23 -0300

    feat: add button router event

---
+++ src/components/BreadCrumbButtons/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="breadcrumb-buttons">
+    <main-button v-for="(button, index) in buttons" :key="index">
+      {{ button.label }}
+    </main-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import MainButton from "../Button.vue";
+import type { BreadcrumbButtons } from "../../types/interfaces";
+
+export default defineComponent({
+  name: "BreadcrumbButtons",
+  components: { MainButton },
+  props: {
+    buttons: { type: Array as () => BreadcrumbButtons[] }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.breadcrumb-buttons {
+  display: flex;
+  gap: 10px;
+}
+</style>
