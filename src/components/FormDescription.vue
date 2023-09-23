7efa074bf512bed6cf4ad9d274c56a1e352835a7
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-09-23 17:27:55 -0300

    refactor: recreate form structure

--- src/components/FormDescription.vue
+++ src/components/FormDescription.vue
@@ -1,21 +1,20 @@
 <template>
   <form @submit.prevent="$emit('submitDescription', value)">
-    <h3>Insira aqui o nome do seu melhor item!</h3>
-    <div class="description-generate">
-      <label>Produto</label>
-      <input type="text" v-model="value.description" placeholder="nome do produto">
-      <p>defina um tamanho para o seu texto:...</p>
-      <button type="submit">Gerar descrição</button>
+    <div class="base-input">
+      <input v-model="value.description" type="text" placeholder="Insira seu melhor produto">
+      <main-button type="submit">Gerar descrição</main-button>
     </div>
   </form>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import MainButton from '../components/Button.vue';
 
 export default defineComponent({
   name: "FormDescription",
   emits: ['submitDescription'],
+  components: { MainButton },
   data() {
     return {
       value: {
@@ -27,66 +26,31 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-form {
-  height: 100%;
+.base-input {
+  background: #fff;
+  width: 600px;
+  border: 1px solid #fff;
+  border-radius: 8px;
+  text-align: start;
+  margin: 0 auto;
+  padding: 0 15px;
+  height: 44px;
+  position: relative;
 
-  h3 {
-    font-size: 1.7rem;
-    color: #0d0d0d;
-    font-weight: 600;
-    margin-bottom: 20px;
-    letter-spacing: -1px;
+  input {
+    font-size: 1.0rem;
+    border: none;
+    outline: none;
+    height: 100%;
+    width: 73%;
   }
-  .description-generate {
-    display: flex;
-    flex-direction: column;
 
-    label {
-      margin-bottom: 5px;
-      color: #0d0d0d;
-      font-weight: normal;
-      font-size: 1.2rem;
-    }
-
-    input {
-      padding: 10px;
-      font-size: 1rem;
-      color: #0d0d0d;
-      height: 45px;
-      border-radius: 8px;
-      outline: none;
-      border: 1px solid #dbdbdb;
-      background-color: #f4f4f4;
-      margin-bottom: 10px;
-      transition: .2s ease-in-out;
-
-      &:focus {
-        border-color: #0d0d0d;
-        background-color: #fafafa;
-      }
-    }
-
-    button {
-      width: 180px;
-      height: 40px;
-      border-radius: 12px;
-      border: 1px solid #0d0d0d;
-      background: #0d0d0d;
-      color: #fff;
-      font-size: 1rem;
-      cursor: pointer;
-      transition: .2s ease-in-out;
-
-      &:hover {
-        border-color: gray;
-        background-color: gray;
-      }
-
-      &:active {
-        background-color: #0d0d0d;
-        transition: 0s;
-      }
-    }
+  button {
+    background: #000;
+    color: #fff;
+    position: absolute;
+    right: -77px;
+    transform: translate(-50%, 2%);
   }
 }
 </style>
